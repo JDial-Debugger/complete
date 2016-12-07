edd36a790cc520c7647291564ae72b2ae7fffe86
Isaac Evavold <isaacevavold@gmail.com>
2016-12-07 15:46:31 -0600

    remove un-used installs

--- provision.sh
+++ provision.sh
@@ -1,8 +1,5 @@
 sudo apt-get update -y
 
-# install dependencies for Sketch
-sudo apt-get install -y build-essential autoconf libtool flex bison mercurial zsh
-
 # install Java 8 JDK
 sudo apt-get install -y software-properties-common python-software-properties
 sudo apt-add-repository ppa:openjdk-r/ppa -y
