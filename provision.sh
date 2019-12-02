d87daf1c47ad9198704696a2eff96a4dc317aca9
Matt <kramer22@wisc.edu>
2019-12-02 10:21:06 -0600

    update Vagrant file to set environment variable for suggest path

--- provision.sh
+++ provision.sh
@@ -1,6 +1,8 @@
-sudo apt-get update -y
+
+echo "export SUGGEST_PATH=/vagrant/backend/suggest/" >> ~/.profile
 
 # install Java 8 JDK
+sudo apt-get update -y
 sudo apt-get install -y software-properties-common python-software-properties
 sudo apt-add-repository ppa:openjdk-r/ppa -y
 sudo apt-get update -y
